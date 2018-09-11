b1d94b39a02c4b9962dc8d4281753a6dada42294
Anders Buron Christiansen <Anders.Buron@gmail.com>
2018-09-11 13:02:48 +0200

    test push

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//test
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
